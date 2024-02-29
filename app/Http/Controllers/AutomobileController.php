ef53d90900e74b81867f764f3582b275bcc03130
soufianeRabie <rabyasoufiane@gmail.com>
2024-02-29 15:50:51 +0100

    some changes

---
+++ app/Http/Controllers/AutomobileController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\automobile;
+use Illuminate\Http\Request;
+
+class AutomobileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(automobile $automobile)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(automobile $automobile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, automobile $automobile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(automobile $automobile)
+    {
+        //
+    }
+}
